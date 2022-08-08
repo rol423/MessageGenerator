4490281465113726e4adf1512ec70476988b139f
Roland Kirchhoff <rolandkirchhoff@web.de>
2022-08-08 10:05:17 +0200

    First final release

---
+++ index.js
@@ -0,0 +1,16 @@
+const names =   ["Jenni", "Sevil", "Robin", "Mike", "Niklas", "Olli", "Roland", "Sarah"];
+const daytime = ["Guten Morgen", "Guten Abend", "Hi", "Hallihallo", "Tach", "Bonjour", "Buenos Dias", "Grüß Gott"];
+
+
+const message = () => {
+    const rand1 =   getRandomInt(0, 7);
+    const rand2 =   getRandomInt(0, 7);
+    const rand3 =   getRandomInt(-10, 35);
+    alert(daytime[rand1] + " " + names[rand2] + "! Die Temperatur heute beträgt " + rand3 + " Grad.");
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
